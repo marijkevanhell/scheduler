d9cb455861a5eea175196203119d557915009ed7
marijkevanhell <jukevanhell@gmail.com>
2023-06-13 20:00:28 -0700

    Update & add state

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,12 +2,37 @@ import React from "react";
 
 import "components/Application.scss";
 
+import DayList from "./DayList";
+
+import React, { useState } from "react";
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
         <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler"/>
         <hr className="sidebar__separator sidebar--centered" />
+        <nav><DayList days={days} day={day} setDay={setDay}/></nav>
         <nav className="sidebar__menu"></nav>
         <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs"/>
       </section>
@@ -17,3 +42,5 @@ export default function Application(props) {
     </main>
   );
 }
+
+
