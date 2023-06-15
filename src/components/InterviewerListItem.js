e7f33cc1f388a5662e835850d1f2ca648dc1e57b
marijkevanhell <jukevanhell@gmail.com>
2023-06-15 14:35:34 -0700

    Add first appointment components & fix prev errors

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -6,7 +6,7 @@ import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
 
-  const { id, name, avatar, selected, setInterviewer} = props;
+  const { name, avatar, selected, setInterviewer} = props;
 
   const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": selected
@@ -18,7 +18,7 @@ export default function InterviewerListItem(props) {
   return (
     <li className={interviewerClass} onClick={setInterviewer}>
     <img
-      className="interviewers__item-image"
+      className={interviewerImgClass}
       src={avatar}
       alt={name}
     />
--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,12 @@ import InterviewerListItem from "components/InterviewerListItem";
 
 import InterviewerList from "components/InterviewerList";
 
+import Appointment from "components/Appointment";
+
+import Header from "components/Appointment/Header";
+
+import Empty from "components/Appointment/Empty";
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -138,4 +144,12 @@ storiesOf("Button", module)
     />
   ));
 
+  storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
   
